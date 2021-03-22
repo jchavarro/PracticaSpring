b5da74ab4f0505906d90d47e9711981dff8d5db8
Juan Camilo Chavarro <j.chavarro@utp.edu.co>
2021-03-22 17:56:29 -0500

    Correccion del RequesParam

---
+++ src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
@@ -0,0 +1,34 @@
+package co.edu.utp.isc.gia.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("hello")
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+        
+        @GetMapping("/{firstName}/{lastName}")
+        public String sayHello(
+                @PathVariable("firstName") String firstName,
+                @PathVariable("lastName") String lastName) {
+            return String.format("<h1>Hello %s %s</h1>", firstName, lastName);
+        }
+        
+        @GetMapping()
+        public String sayHelloRequest(
+                @RequestParam("firstName") String firstName,
+                @RequestParam("lastName") String lastName) {
+            return String.format("<h2>Hello %s %s</h2>", firstName, lastName);
+        }
+        
+}
